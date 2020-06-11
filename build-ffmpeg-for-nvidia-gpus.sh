8e2f35c608c28931f8158730b977f738e0076968
Kim Erik Tjora Olsen <post@kimeriktjoraolsen.no>
2020-06-11 20:22:54 +0200

    Add compile-scripts

---
+++ build-ffmpeg-for-nvidia-gpus.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script to build FFmpeg for Nvidia GPUs, utilizing NVenc encoder/decoder
+# Using: nv-codec-headers + ffmpeg git repositories
+#
+# Remove ubuntu-standard ffmpeg
+sudo apt-get remove --purge ffmpeg
+# Clone latest ffmpeg git-repo
+git clone https://git.ffmpeg.org/ffmpeg.git
+# Clone latest NV-codec-headers (Nvidia)
+git clone https://github.com/FFmpeg/nv-codec-headers
+cd nv-codec-headers/
+sudo make install
+cd ../
+# Build and install ffmpeg (for Nvidia GPUs)
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
+cd ffmpeg/
+./configure --enable-cuda --enable-cuvid --enable-nvenc --enable-nonfree --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --enable-shared --enable-gpl --enable-version3 --enable-runtime-cpudetect --disable-opencl --enable-libmp3lame --enable-libx264 --cc="gcc -m64 -fPIC"
+# Compile with 8 CPU threads 
+make -j 8 -s
+# Build and install Ubuntu/Debian .deb-package
+sudo checkinstall --pkgname=FFmpeg
+sudo apt-mark hold ffmpeg
+# Run linker and make symlink to ffmpeg
+sudo ldconfig && sudo ln -s /usr/local/bin/ffmpeg /usr/bin/ffmpeg
+cd ../
---
+++ build-obs-studio-for-nvidia-gpus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to build OBS-studio for use with NVIDIA NVENC encoder/decoder chip
+# Add obs-studio PPA
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt-get update
+# Get obs-studio source
+apt-get source obs-studio
+cd obs-studio-25.0.8/
+# Make build-directory
+mkdir build && cd build
+# Run cmake to generate configuration
+cmake -DUNIX_STRUCTURE=1 -DCMAKE_INSTALL_PREFIX=/usr ..
+# Compile OSB-studio with 8 CPU threads
+make -j 8 -s
+# Make Debian package
+sudo checkinstall --pkgname=obs-studio
+# Mark obs-studio package on hold
+sudo apt-mark hold obs-studio
