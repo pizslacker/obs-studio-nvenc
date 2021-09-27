2bb89fbb21b0e700c8130a75eeef61c85704e078
Kim Olsen <kim@pizslacker.org>
2021-09-27 22:25:50 +0200

    re-added CUDA install scripts

---
+++ install-cuda-10.2-sdk-ubuntu-18.04.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Script to install Nvidia CUDA v10.2 SDK + cuda-driver on Ubuntu 18.04
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
+sudo apt-mark hold cuda*
