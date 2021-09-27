c1f8bfe6042262d872d5cfcaf5223a1c22597c9b
Kim Olsen <kim@pizslacker.org>
2021-09-27 22:22:06 +0200

    no need to build obs-studio anymore

---
+++ install-obs-studio-for-nvidia-gpus.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script to install latest OBS-studio for use with NVIDIA NVENC encoder/decoder chip
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install obs-studio
